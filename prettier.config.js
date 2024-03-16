51cf5bff45c9b87ac4bf2099dbf8a66951d5f6dc
Alvis Kalarikkan <alvis.kalarikkan@gmail.com>
2024-03-16 12:52:51 +0000

    Add VS Code settings and update Prettier config

--- prettier.config.js
+++ prettier.config.js
@@ -1,5 +1,6 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 const config = {
+  singleQuote: true,
   plugins: ["prettier-plugin-tailwindcss"],
 };
 
--- src/app/wagmi-layout.tsx
+++ src/app/wagmi-layout.tsx
@@ -1,25 +1,25 @@
-'use client'
+"use client";
 
-import '@rainbow-me/rainbowkit/styles.css'
+import "@rainbow-me/rainbowkit/styles.css";
 
 import {
   RainbowKitProvider,
   darkTheme,
   getDefaultConfig,
-} from '@rainbow-me/rainbowkit'
+} from "@rainbow-me/rainbowkit";
 
 import {
   type GetSiweMessageOptions,
   RainbowKitSiweNextAuthProvider,
-} from '@rainbow-me/rainbowkit-siwe-next-auth'
+} from "@rainbow-me/rainbowkit-siwe-next-auth";
 
-import { SessionProvider } from 'next-auth/react'
-import { WagmiProvider, http } from 'wagmi'
-import * as wagmiChains from 'wagmi/chains'
+import { SessionProvider } from "next-auth/react";
+import { WagmiProvider, http } from "wagmi";
+import * as wagmiChains from "wagmi/chains";
 
-import { type FC, type PropsWithChildren } from 'react'
+import { type FC, type PropsWithChildren } from "react";
 
-import { env } from '~/env'
+import { env } from "~/env";
 
 const metadata = {
   title: "Paid2Hack",
@@ -27,16 +27,16 @@ const metadata = {
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
-import { ConnectWallet, Disclaimer } from '~/app/Components/UI/ConnectWallet'
+import { ConnectWallet, Disclaimer } from "~/app/Components/UI/ConnectWallet";
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import Link from 'next/link'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Link from "next/link";
 
 const getSiweMessageOptions: GetSiweMessageOptions = () => ({
   statement: `Sign in to ${metadata.title}`,
-})
+});
 
-const chain = (wagmiChains as any)[env.NEXT_PUBLIC_CHAIN]
+const chain = (wagmiChains as any)[env.NEXT_PUBLIC_CHAIN];
 
 const config = getDefaultConfig({
   appName: metadata.title,
@@ -46,16 +46,18 @@ const config = getDefaultConfig({
     [chain.id]: http(env.NEXT_PUBLIC_CHAIN_RPC_ENDPOINT),
   },
   ssr: true,
-})
+});
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 export const WagmiLayout: FC<PropsWithChildren<{}>> = ({ children }) => {
   return (
     <WagmiProvider config={config}>
       <SessionProvider refetchInterval={0}>
         <QueryClientProvider client={queryClient}>
-          <RainbowKitSiweNextAuthProvider getSiweMessageOptions={getSiweMessageOptions}>
+          <RainbowKitSiweNextAuthProvider
+            getSiweMessageOptions={getSiweMessageOptions}
+          >
             <RainbowKitProvider
               theme={darkTheme()}
               initialChain={chain}
@@ -64,24 +66,19 @@ export const WagmiLayout: FC<PropsWithChildren<{}>> = ({ children }) => {
                 disclaimer: Disclaimer,
               }}
             >
-              <main className="relative min-h-screen bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+              <main className="relative min-h-screen bg-black text-white">
                 <div className="absolute left-2 top-2">
                   <Link href="/">Home</Link>
                 </div>
                 <div className="absolute right-2 top-2">
                   <ConnectWallet />
                 </div>
-                <div className="relative top-10 p-4">
-                  {children}
-                </div>
+                <div className="relative top-10 p-4">{children}</div>
               </main>
             </RainbowKitProvider>
           </RainbowKitSiweNextAuthProvider>
         </QueryClientProvider>
       </SessionProvider>
     </WagmiProvider>
-  )
-}
-
-
-
+  );
+};
