5a78ef8615b207679a3e3446f659dc4fd978a692
Ramesh Nair <ram@hiddentao.com>
2024-03-16 12:31:27 +0000

    feat: create team basic dialog

---
+++ src/app/Components/UI/CreateTeamDialog.tsx
@@ -0,0 +1,82 @@
+"use client"
+
+import { FC, PropsWithChildren, ReactNode, useCallback, useMemo, useState } from "react";
+import { ActionDialog, CloseDialogCallback } from "./ActionDialog";
+import { useWriteContract } from "wagmi";
+import { MASTER_CONTRACT_CONFIG } from "~/contracts";
+import { Button } from "./Button";
+import { ErrorBox } from "./ErrorBox";
+import { EventInfo, LoadEventInfo } from "./LoadEventInfo";
+import { useWallet } from "~/app/hooks/wallet";
+
+const Form: FC<{ id: number, info: EventInfo, closeDialog: CloseDialogCallback }> = ({ id, info, closeDialog }) => {
+  const wallet = useWallet()
+  const { writeContractAsync } = useWriteContract()
+
+  const [name, setName] = useState('');
+  const [creating, setCreating] = useState(false);
+  const [error, setError] = useState('');
+
+  // TODO: input team member addresses
+
+  const onNameChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  }, [])
+
+  const canSubmit = useMemo(() => {
+    return name.length > 0 && name.length < 40 && !creating
+  }, [name, creating])
+
+  const onSubmit = useCallback(async (e: React.FormEvent<HTMLFormElement>, closeDialog: CloseDialogCallback) => {
+    e.preventDefault();
+
+    if (!canSubmit) {
+      return;
+    }
+
+    try {
+      setCreating(true)
+
+      await writeContractAsync({
+        ...MASTER_CONTRACT_CONFIG,
+        functionName: 'createTeam',
+        args: [BigInt(id), {
+          name,
+          leader: wallet!.address,
+          members: []
+        }],
+      })
+
+      closeDialog()
+    } catch (err: any) {
+      console.error(err)
+      setError(err.message)
+    } finally {
+      setCreating(false)
+    }
+  }, [canSubmit, id, name, wallet, writeContractAsync])
+
+  return (
+    <form className="flex flex-col" onSubmit={(e) => onSubmit(e, closeDialog)}>
+      <input className="text-black mb-4" type="text" placeholder="Event name" onChange={onNameChange} value={name} max={40} size={30} />
+      <p className="my-4">Team leader: {wallet?.address}</p>
+      <Button className="mb-2" type="submit" disabled={!canSubmit}>Create team</Button>
+      {error ? <ErrorBox>{error}</ErrorBox> : null}
+    </form>
+  )
+}
+
+
+export const CreateTeamDialog: FC<PropsWithChildren<{ eventId: number }>> = ({ eventId, children }) => {
+  return (
+    <ActionDialog
+      renderContent={(closeDialog) => (
+        <LoadEventInfo eventId={eventId}>
+          {(ev) => <Form id={eventId} info={ev} closeDialog={closeDialog} />}
+        </LoadEventInfo>
+      )}
+    >
+      {children}
+    </ActionDialog>
+  )
+}
--- src/app/event/[id]/page.tsx
+++ src/app/event/[id]/page.tsx
@@ -1,7 +1,9 @@
 "use client"
 
 import { Button } from "~/app/Components/UI/Button"
+import { CreateTeamDialog } from "~/app/Components/UI/CreateTeamDialog"
 import { IfEventCreator } from "~/app/Components/UI/IfEventCreator"
+import { IfWalletConnected } from "~/app/Components/UI/IfWalletConnected"
 import { LoadEventInfo } from "~/app/Components/UI/LoadEventInfo"
 import { UpdateEventNameDialog } from "~/app/Components/UI/UpdateEventNameDialog"
 
@@ -12,12 +14,20 @@ export default function EventPage({ params }: { params: { id: number } }) {
     <LoadEventInfo eventId={id}>
       {(ev) => (
         <div>
-          <h1>Event: {ev.name}</h1>
+          <h1>Event: {ev.name} (id: {id})</h1>
+          <p className="my-2">Creator: {ev.owner}</p>
           <IfEventCreator eventId={id}>
             <UpdateEventNameDialog eventId={id}>
               <Button className="mb-2">Update event name</Button>
             </UpdateEventNameDialog>
           </IfEventCreator>
+          <div className="mt-10">
+            <IfWalletConnected connectButton={null}>
+              <CreateTeamDialog eventId={id}>
+                <Button>Create team</Button>
+              </CreateTeamDialog>
+            </IfWalletConnected>
+          </div>
         </div>
       )}
     </LoadEventInfo>
