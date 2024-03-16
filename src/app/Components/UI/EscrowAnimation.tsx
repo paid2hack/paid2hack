1603e42dcbbb5cefb3957c240e99918f3ab63101
Alvis Kalarikkan <alvis.kalarikkan@gmail.com>
2024-03-16 19:36:05 +0000

    Turn animation time based, change svg paths

--- src/app/Components/UI/EscrowAnimation.tsx
+++ src/app/Components/UI/EscrowAnimation.tsx
@@ -1,159 +1,114 @@
-"use client";
-import { cn } from "~/app/lib/utils";
-import { motion, MotionValue } from "framer-motion";
-import React from "react";
-import { Lock } from "lucide-react";
+'use client';
+import { cn } from '~/app/lib/utils';
+import { motion, useAnimation } from 'framer-motion';
+import React, { useEffect } from 'react';
+import { Lock, Unlock, Wand } from 'lucide-react';
+import { Button } from './Button';
+import { CreateEventDialog } from '../Dialogs/CreateEventDialog';
+
 const transition = {
-  duration: 0,
-  ease: "linear",
+  duration: 1,
+  ease: 'easeInOut',
 };
 
 export const EscrowAnimation = ({
-  pathLengths,
   title,
   description,
   className,
 }: {
-  pathLengths: MotionValue[];
   title?: string;
   description?: string;
   className?: string;
 }) => {
+  const pathControls = useAnimation();
+
+  useEffect(() => {
+    const animateLines = async () => {
+      await pathControls.start((i) => ({
+        pathLength: 1,
+        transition: { duration: 1, ease: 'easeInOut', delay: i * 0.2 },
+      }));
+    };
+
+    animateLines();
+  }, [pathControls]);
+
   return (
-    <div className={cn("sticky top-80", className)}>
+    <div className={cn('sticky top-10', className)}>
       <h1 className="text-center text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
         {title || (
-          <>
-            &lt; Paid<span className="text-[hsl(280,100%,70%)]">2</span>
-            Hack&#47; &gt;
-          </>
+          <div className="flex w-full items-center justify-center gap-2">
+            <div className="text-zinc-400">&lt; </div>
+            <div className="tracking-tight">
+              Paid<span className="text-[hsl(280,100%,70%)]">2</span>
+              Hack
+            </div>
+            <div className="text-zinc-400">&#47;&gt;</div>
+          </div>
         )}
       </h1>
       <p className="mx-auto mt-4 max-w-lg text-center text-xs font-normal text-neutral-400 md:text-xl">
-        {description ||
-          `Scroll this component and see the bottom SVG come to life wow this
-        works!`}
+        {description || `Making sure builders get paid since 2024`}
       </p>
-      <div className="bg-red-transparent absolute -top-60 flex h-[890px] w-full items-center justify-center md:-top-40">
-        {" "}
-        <button className="z-30 mx-auto w-fit rounded-full bg-white px-2 py-1 text-xs font-bold text-black md:px-4 md:py-2 md:text-base">
-          {" "}
-          ui.aceternity.com{" "}
-        </button>{" "}
-      </div>{" "}
-      <svg
-        width="1440"
-        height="890"
-        viewBox="0 0 1440 890"
-        xmlns="http://www.w3.org/2000/svg"
-        className="absolute -top-60 w-full md:-top-40"
-      >
-        <motion.path
-          d="M0 738C200 738 400 600 720 600C1040 600 1240 738 1440 738"
-          stroke="#FFB7C5"
-          strokeWidth="2"
-          fill="none"
-          initial={{
-            pathLength: 0,
-          }}
-          style={{
-            pathLength: pathLengths[0],
-          }}
-          transition={transition}
-        />
-        <motion.path
-          d="M0 663C200 663 400 600 720 600C1040 600 1240 663 1440 663"
-          stroke="#FFDDB7"
-          strokeWidth="2"
-          fill="none"
-          initial={{
-            pathLength: 0,
-          }}
-          style={{
-            pathLength: pathLengths[1],
-          }}
-          transition={transition}
-        />
-        <motion.path
-          d="M0 588C200 588 400 600 720 600C1040 600 1240 588 1440 588"
-          stroke="#B1C5FF"
-          strokeWidth="2"
-          fill="none"
-          initial={{
-            pathLength: 0,
-          }}
-          style={{
-            pathLength: pathLengths[2],
-          }}
-          transition={transition}
-        />
-        <motion.path
-          d="M0 513C200 513 400 600 720 600C1040 600 1240 513 1440 513"
-          stroke="#4FABFF"
-          strokeWidth="2"
-          fill="none"
-          initial={{
-            pathLength: 0,
-          }}
-          style={{
-            pathLength: pathLengths[3],
-          }}
-          transition={transition}
-        />
-        <motion.path
-          d="M0 438C200 438 400 600 720 600C1040 600 1240 438 1440 438"
-          stroke="#076EFF"
-          strokeWidth="2"
-          fill="none"
-          initial={{
-            pathLength: 0,
-          }}
-          style={{
-            pathLength: pathLengths[4],
-          }}
-          transition={transition}
-        />
-        <path
-          d="M0 738C200 738 400 600 720 600C1040 600 1240 738 1440 738"
-          stroke="#FFB7C5"
-          strokeWidth="2"
-          fill="none"
-          filter="url(#blurMe)"
-        />
-        <path
-          d="M0 663C200 663 400 600 720 600C1040 600 1240 663 1440 663"
-          stroke="#FFDDB7"
-          strokeWidth="2"
-          fill="none"
-          filter="url(#blurMe)"
-        />
-        <path
-          d="M0 588C200 588 400 600 720 600C1040 600 1240 588 1440 588"
-          stroke="#B1C5FF"
-          strokeWidth="2"
-          fill="none"
-          filter="url(#blurMe)"
-        />
-        <path
-          d="M0 513C200 513 400 600 720 600C1040 600 1240 513 1440 513"
-          stroke="#4FABFF"
-          strokeWidth="2"
-          fill="none"
-          filter="url(#blurMe)"
-        />
-        <path
-          d="M0 438C200 438 400 600 720 600C1040 600 1240 438 1440 438"
-          stroke="#076EFF"
-          strokeWidth="2"
-          fill="none"
-          filter="url(#blurMe)"
-        />
-        <defs>
-          <filter id="blurMe">
-            <feGaussianBlur in="SourceGraphic" stdDeviation="5" />
-          </filter>
-        </defs>
-      </svg>
+      <div className="relative mt-8">
+        <div className="absolute inset-0 flex items-center justify-center">
+          <div className="absolute z-30 mx-auto flex w-fit rounded-full bg-white px-2 py-1 text-xs font-bold text-black md:px-4 md:py-2 md:text-base">
+            <CreateEventDialog>
+              <Button asChild={true} className="">
+                <div className="flex w-fit">
+                  Create Event <Wand className="ml-2 h-[24] w-[24]" />
+                </div>
+              </Button>
+            </CreateEventDialog>
+          </div>
+        </div>
+        <svg
+          width="1440"
+          height="400"
+          viewBox="0 0 1440 400"
+          xmlns="http://www.w3.org/2000/svg"
+          className="w-full"
+        >
+          {[
+            'M0 338C200 338 400 200 720 200C1040 200 1240 338 1440 338',
+            'M0 263C200 263 400 200 720 200C1040 200 1240 263 1440 263',
+            'M0 188C200 188 400 200 720 200C1040 200 1240 188 1440 188',
+            'M0 113C200 113 400 200 720 200C1040 200 1240 113 1440 113',
+            'M0 38C200 38 400 200 720 200C1040 200 1240 38 1440 38',
+          ].map((d, i) => (
+            <React.Fragment key={i}>
+              <motion.path
+                d={d}
+                stroke={
+                  ['#FFB7C5', '#FFDDB7', '#B1C5FF', '#4FABFF', '#076EFF'][i]
+                }
+                strokeWidth="2"
+                fill="none"
+                initial={{
+                  pathLength: 0,
+                }}
+                animate={pathControls}
+                custom={i}
+                transition={transition}
+              />
+              <path
+                d={d}
+                stroke={
+                  ['#FFB7C5', '#FFDDB7', '#B1C5FF', '#4FABFF', '#076EFF'][i]
+                }
+                strokeWidth="2"
+                fill="none"
+                filter="url(#blurMe)"
+              />
+            </React.Fragment>
+          ))}
+          <defs>
+            <filter id="blurMe">
+              <feGaussianBlur in="SourceGraphic" stdDeviation="5" />
+            </filter>
+          </defs>
+        </svg>
+      </div>
     </div>
   );
 };
